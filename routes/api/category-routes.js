bf9e5e77f7f9d8a0d8aac0a289811ba401a0b4ea
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-07-25 11:58:43 -0600

    Completed models, began building routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,55 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    // find all categories
+    // be sure to include its associated Products
+    Category.findAll({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    Category.findOne({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+    // create a new category
+    Category.create({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    // update a category by its `id` value
+    Category.update({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+    // delete a category by its `id` value
+    Category.destroy({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,25 +4,60 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+    // find all tags
+    // be sure to include its associated Product data
+    console.log('==========');
+    Tag.findAll({
+            attributes: [],
+            order: [],
+            include: [],
+        })
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
+    Tag.findOne({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+    // create a new tag
+    Tag.create({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+    // update a tag's name by its `id` value
+    Tag.update({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+    // delete on tag by its `id` value
+    Tag.destroy({})
+        .then(() => res.json())
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
