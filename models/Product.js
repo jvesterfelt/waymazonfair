999c5311f8578630d7fb80682b741d9d28f5bba2
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-07-25 18:29:50 -0600

    Completed all routes and tested functionality.

--- models/Product.js
+++ models/Product.js
@@ -29,8 +29,8 @@ Product.init({
     stock: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        defaultValue: 10,
         validate: {
-            len: [10],
             isInt: true
         }
     },
